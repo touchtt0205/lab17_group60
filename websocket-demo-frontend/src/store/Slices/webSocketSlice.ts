e418d77d9568a9bb48c293d5e42fba2325afeea8
touch <tttouch1122@gmail.com>
2024-02-19 02:31:48 +0700

    100%

--- websocket-demo-frontend/src/store/Slices/webSocketSlice.ts
+++ websocket-demo-frontend/src/store/Slices/webSocketSlice.ts
@@ -12,6 +12,7 @@ interface webSocketMessage {
     content: string;
     timestamp: string;
     type: messageType;
+    previousOnline: number;
 }
 interface webSocketState {
     isConnected: boolean;
