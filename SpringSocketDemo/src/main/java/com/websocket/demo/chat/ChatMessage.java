847b2362d6cf188da9b25ffb55b082fee52a35ba
touch <tttouch1122@gmail.com>
2024-02-19 02:22:05 +0700

    100%

--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
@@ -7,8 +7,22 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ChatMessage {
+    @Getter
+    static private int countOnline = 0;
+    private int previousOnline;
     private String content;
     private String timestamp;
     private String sender;
     private MessageType type;
+
+    public static void Count(int i){
+        countOnline += i;
+    }
+    public static void addCount(){
+            countOnline += 1;
+    }
+    public static void deleteCount(){
+        countOnline -= 1;
+    }
+    public void setEx(){previousOnline = countOnline;}
 }
